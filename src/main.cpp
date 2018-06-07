f68e49d6184c4608500798e822f8521ff40624d0
neinsys <sys7961@gmail.com>
2018-06-07 22:20:45 +0900

    find_path 라이브러리 연결

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "crow.h"
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "flowgraph.h"
+#include "find_path.h"
 using std::string;
 using std::ofstream;
 using std::endl;
@@ -12,25 +12,6 @@ using std::endl;
 
 
 int main(int argc, char** argv){
-
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-
-	if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[1], *cloud) == -1) //* load the file
-	{
-		PCL_ERROR ("Couldn't read file %s \n", argv[1]);
-		return (-1);
-	}
-	string filePath = argv[2];
-	// write File
-	ofstream writeFile(filePath.data());
-	if( writeFile.is_open() ){
-		writeFile << cloud->width * cloud->height << endl;
-		for (size_t i = 0; i < cloud->points.size (); ++i) {
-			writeFile << cloud->points[i].x << " " << cloud->points[i].y 
-			<< " " << cloud->points[i].z << endl;
-		}
-		writeFile.close();
-	}
     crow::SimpleApp app;
     CROW_ROUTE(app,"/")
     ([]{
